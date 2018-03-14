94cb29d6944edddc98b90dfa14573ca1dd9eecc6
leo424y <leo424y@gmail.com>
2018-03-14 11:24:20 +0800

    add page for demo

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root to: "admin/dashboard#index"
+
+  get "/index.html" => "static#indexkp"
 end
