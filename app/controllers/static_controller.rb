32e4eb21ab4f6e9cb24d91df6f20e05b75a7f1c9
leo424y <leo424y@gmail.com>
2018-04-03 13:56:20 +0800

    update github file ok

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -4,20 +4,31 @@ require 'net/http'
 class StaticController < ApplicationController
   def editor
     # link github
-    @github_code = get_github 'twgo', 'gi2-gian5_boo5-hing5', 'Dockerfile'
+    params[:branch] = 'siann0102'
+    @github_code = params[:branch] ? (get_dockerfile 'gi2-gian5_boo5-hing5', params[:branch]) : ''
     # read data
   end
 
   def update
+    params[:branch] = 'siann0102'
+    params[:repo] = 'gi2-gian5_boo5-hing5'
 
+    contents = Github::Client::Repos::Contents.new oauth_token: ENV['GITHUB_TOKEN']
+    file = contents.get 'twgo', params[:repo], 'Dockerfile', ref: params[:branch]
+    contents.update('twgo', params[:repo], 'Dockerfile', path: 'Dockerfile', branch: params[:branch], message: 'EXP commit', content: params[:github_code][:content], sha: file.sha)
+    redirect_to rounds_path
   end
 
   private
 
-  def get_github repo, project, file
-    contents = Github::Client::Repos::Contents.new oauth_token: ENV['GITHUB_TOKEN']
-    code = contents.get repo, project, file
+  def get_dockerfile project, branch
+    # url = "https://raw.githubusercontent.com/twgo/#{project}/#{branch}/Dockerfile"
+    #
+    # Net::HTTP.get(URI.parse(URI.escape(url))).force_encoding("UTF-8")
 
-    Net::HTTP.get(URI.parse(code.download_url)).force_encoding("UTF-8")
+    contents = Github::Client::Repos::Contents.new oauth_token: ENV['GITHUB_TOKEN']
+    url = contents.get(user: 'twgo', repo: project, path: 'Dockerfile', ref: branch).download_url
+
+    Net::HTTP.get(URI.parse(URI.escape(url))).force_encoding("UTF-8")
   end
 end
