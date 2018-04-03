6384bde59b6db7dc74a38be6adc74b6565694a62
leo424y <leo424y@gmail.com>
2018-04-03 11:01:19 +0800

    better view, try load twgo ok

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -12,7 +12,7 @@ class StaticController < ApplicationController
     require 'net/http'
 
     contents = Github::Client::Repos::Contents.new oauth_token: ENV['GITHUB_TOKEN']
-    code = contents.get 'leo424y', 'playground', 'README.md'
+    code = contents.get 'twgo', 'gi2-gian5_boo5-hing5', 'Dockerfile'
 
     Net::HTTP.get(URI.parse(code.download_url)).force_encoding("UTF-8")
   end
