5be2634b9a1b9d43bc65a68f352cde621ab43c75
leo424y <leo424y@gmail.com>
2018-04-10 15:15:35 +0800

    add model test: combine round string

--- spec/models/round_spec.rb
+++ spec/models/round_spec.rb
@@ -1,5 +1,10 @@
-# require 'rails_helper'
-#
-# RSpec.describe Round, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+require 'rails_helper'
+
+RSpec.describe Round, type: :model do
+  describe "jid_info" do
+    it 'returns an jid_info string' do
+      Round.create(jid: 'exp/1', info: 'test', label: 'more info')
+      expect(Round.first.jid_info).to eq 'exp/1 - test - more info'
+    end
+  end
+end
