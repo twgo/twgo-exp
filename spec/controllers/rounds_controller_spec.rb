993d69ac6e150dbf234958c68f292c8579fc790c
leo424y <leo424y@gmail.com>
2018-04-10 15:31:26 +0800

    add spec: update label info with round id and label text

--- spec/controllers/rounds_controller_spec.rb
+++ spec/controllers/rounds_controller_spec.rb
@@ -8,4 +8,11 @@ RSpec.describe RoundsController, type: :controller do
       expect(response).to be_success
     end
   end
+  describe "#update" do
+    it 'update label of a round' do
+      Round.create(jid: 'exp/1', info: 'test', label: 'info')
+      post :update, params: {id: 1, round: {id: 1, label: 'update info'}}
+      expect(Round.first.label).to eq 'update info'
+    end
+  end
 end
