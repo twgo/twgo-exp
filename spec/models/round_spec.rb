e67f69bbe103b49afa4556ac557adfb38c3ff56f
leo424y <leo424y@gmail.com>
2018-05-11 11:10:29 +0800

    add branch to round info

--- spec/models/round_spec.rb
+++ spec/models/round_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe Round, type: :model do
   describe "#jid_info" do
     it 'returns an jid_info string' do
-      Round.create(jid: 'exp/1', info: 'test', label: 'more info')
-      expect(Round.first.jid_info).to eq 'exp/1 - test - more info'
+      Round.create(jid: 'exp/1', info: 'test', label: 'more info', branch: 'branch')
+      expect(Round.first.jid_info).to eq 'exp/1 - branch - test - more info'
     end
   end
 end
