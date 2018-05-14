3c45758471ad8e13ecbca4e20ed6091d47c87296
leo424y <leo424y@gmail.com>
2018-05-14 09:03:35 +0800

    fix: spec with input info

--- spec/workers/jenkins_worker_spec.rb
+++ spec/workers/jenkins_worker_spec.rb
@@ -4,9 +4,12 @@ require './app/workers/jenkins_worker'
 RSpec.describe JenkinsWorker, type: :worker do
   describe "GET #exp_rate" do
     it "returns tri4 no si rate" do
+      input_repo = 'siann1-hak8_boo5-hing5'
+      input_exp_id = '59'
+      input_status = 'SUCCESS'
       my_object = JenkinsWorker.new
-      origin_code = my_object.send(:exp_rate, 'siann1-hak8_boo5-hing5', '59', 'SUCCESS')
-      expect(origin_code).to eq '29.83'
+      my_exp_rate = my_object.send(:exp_rate, input_repo, input_exp_id, input_status)
+      expect(my_exp_rate).to eq '29.83'
     end
   end
 end
