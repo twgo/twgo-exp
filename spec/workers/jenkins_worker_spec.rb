7562651fc28692bfb16fa3cec555b874e74bd538
leo424y <leo424y@gmail.com>
2018-05-15 14:47:33 +0800

    stub connection with ci

--- spec/workers/jenkins_worker_spec.rb
+++ spec/workers/jenkins_worker_spec.rb
@@ -3,14 +3,25 @@ require './app/workers/jenkins_worker'
 
 RSpec.describe JenkinsWorker, type: :worker do
   describe "GET #exp_rate" do
-    # todo stub
-    # it "returns tri4 no si rate" do
-    #   input_repo = 'siann1-hak8_boo5-hing5'
-    #   input_exp_id = '59'
-    #   input_status = 'SUCCESS'
-    #   my_object = JenkinsWorker.new
-    #   my_exp_rate = my_object.send(:exp_rate, input_repo, input_exp_id, input_status)
-    #   expect(my_exp_rate).to eq '29.83'
-    # end
+    before do
+      @reop = 'siann1-hak8_boo5-hing5'
+      @exp_id = '888'
+      @round_success_rate = 123.0
+      @round_no_print_rate_code = 0.0
+      @round_fail_rate_code = 999.0
+      @round_running_rate_code = 888.0
+      @worker = JenkinsWorker.new
+    end
+    it "returns rate when success" do
+      @worker = double
+      allow(@worker).to receive(:exp_rate).and_return @round_success_rate
+      expect(@worker.exp_rate).to eq @round_success_rate
+    end
+    it "returns code 999.0 when fail" do
+      expect(@worker.send(:exp_rate, @repo, @exp_id, 'FAILURE')).to eq @round_fail_rate_code
+    end
+    it "returns code 888.0 rate when running" do
+      expect(@worker.send(:exp_rate, @repo, @exp_id, '')).to eq @round_running_rate_code
+    end
   end
 end
