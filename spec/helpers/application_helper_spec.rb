c8d2ba6653471db313f1d5ef4e0e98fa9492b48c
leo424y <leo424y@gmail.com>
2018-05-15 14:01:28 +0800

    mv: make it clearly

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -11,17 +11,23 @@ RSpec.describe ApplicationHelper, :type => :helper do
   end
 
   describe "#show_rate_status(rate)" do
+    before do
+      @round_success = Round.create(rate: 123.0)
+      @round_no_print = Round.create(rate: 0.0)
+      @round_fail = Round.create(rate: 999.0)
+      @round_running = Round.create(rate: 888.0)
+    end
     it "returns 失敗" do
-      expect(helper.show_rate_status(999.0)).to eq '失敗'
+      expect(helper.show_rate_status(@round_fail.rate)).to eq '失敗'
     end
     it "returns 無印結果" do
-      expect(helper.show_rate_status(0.0)).to eq '無印結果'
+      expect(helper.show_rate_status(@round_no_print.rate)).to eq '無印結果'
     end
     it "returns 正在跑" do
-      expect(helper.show_rate_status(888.0)).to eq '正在跑'
+      expect(helper.show_rate_status(@round_running.rate)).to eq '正在跑'
     end
     it "returns rate" do
-      expect(helper.show_rate_status(123.0)).to eq 123.0
+      expect(helper.show_rate_status(@round_success.rate)).to eq 123.0
     end
   end
 end
