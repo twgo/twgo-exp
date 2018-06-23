d874faaf6481ab891074e5b9ae5df1341180c51e
leo424y <leo424y@gmail.com>
2018-06-23 10:52:48 +0800

    Time.zone

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module TwgoExp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.time_zone = 'Asia/Taipei'
     config.i18n.default_locale = "zh-TW"
     config.my_hidden_branches = [
       'exp-testing',
